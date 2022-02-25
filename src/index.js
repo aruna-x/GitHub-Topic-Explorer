7e63331c5b22778d1fe0f2270cea2a1f81f31a3f
Aruna <aruna.evan15@gmail.com>
2022-02-24 17:35:06 -0800

    add redux-persist to save breadcrumbs on refresh

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/client";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 // Components, Modules, Styles
 import App from "./components/App";
 import { client } from "./modules/apolloClient";
-import { store } from './redux/store';
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,38 @@
 // Libraries
-import { createStore } from 'redux';
+import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 let initialState = {
-    topic: "react",
-    breadcrumbs: [],
-}
-  
-function reducer(state=initialState, action) {
-    switch(action.type) {
-        case "SET_TOPIC":
-            return {...state, topic: action.payload};
-        case "ADD_BREADCRUMB":
-            return {...state, breadcrumbs: [...state.breadcrumbs, action.payload]};
-        case "BREADCRUMB_CLICK":
-            return {...state, breadcrumbs: action.payload};
-        default:
-            return state;
-    }
-}
-  
-const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+  topic: "react",
+  breadcrumbs: [],
+};
 
-export { store };
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "SET_TOPIC":
+      return { ...state, topic: action.payload };
+    case "ADD_BREADCRUMB":
+      return { ...state, breadcrumbs: [...state.breadcrumbs, action.payload] };
+    case "BREADCRUMB_CLICK":
+      return { ...state, breadcrumbs: action.payload };
+    default:
+      return state;
+  }
+}
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(
+  persistedReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const persistor = persistStore(store);
+
+export { store, persistor };
